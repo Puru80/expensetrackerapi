e2fdc1bec6c31b84c3d0e917f996a91468e1122d
Puru80 <puru.agar99@gmail.com>
2021-02-10 11:54:17 +0530

    Commit 10 Feb 2021

--- src/main/java/com/expensetracker/expensetrackerapi/repositories/UserRepositoriesImpl.java
+++ src/main/java/com/expensetracker/expensetrackerapi/repositories/UserRepositoriesImpl.java
@@ -1,10 +1,14 @@
 package com.expensetracker.expensetrackerapi.repositories;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
 import com.expensetracker.expensetrackerapi.domain.User;
 import com.expensetracker.expensetrackerapi.exceptions.EtAuthException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -14,6 +18,11 @@ public class UserRepositoriesImpl implements UserRepositories
 {
     private static final String SQL_CREATE = "INSERT INTO ET_USERS(USER_ID, FIRST_NAME, LAST_NAME, "+
     "EMAIL, PASSWORD) VALUES (NEXTVAL('ET_USERS_SEQ'), ?, ?, ?, ?)";
+    private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM ET_USERS WHERE EMAIL = ?";
+    private static final String SQL_FIND_BY_ID = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL PASSWORD"
+    + "FROM ET_USERS WHERE USER_ID = ?";
+    private static final String SQL_FIND_BY_EMAIL = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL,"+
+    " PASSWORD WHERE EMAIL= ?";
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -22,29 +31,46 @@ public class UserRepositoriesImpl implements UserRepositories
     public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
         try {
             KeyHolder keyHolder = new GeneratedKeyHolder();
-            // jdbcTemplate
+            jdbcTemplate.update(connection->{
+                PreparedStatement ps = connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, firstName);
+                ps.setString(2, lastName);
+                ps.setString(3, email);
+                ps.setString(4, password);
+                return ps;
+            }, keyHolder);
+            return (Integer)keyHolder.getKeys().get("USER_ID");
         } catch (Exception e) {
             throw new EtAuthException("Invalid Details");
         }
-        return null;
+        // return null;
     }
 
     @Override
     public User findByEmailAndPassword(String email, String password) throws EtAuthException {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Integer getCountByEmail(String email) {
-        // TODO Auto-generated method stub
-        return null;
+        // return null;
+        return jdbcTemplate.queryForObject(SQL_COUNT_BY_EMAIL, Integer.class, new Object[]{email});
+        // int i = jdbcTemplate.queryForObject(sql, args, requiredType)
     }
 
     @Override
     public User findById(Integer userId) {
-        // TODO Auto-generated method stub
-        return null;
+        // return null;
+        return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, userRowMapper, new Object[]{userId});
     }
+
+    //Purpose - Take a result set and return a proper User object
+    private RowMapper<User> userRowMapper = ((rs, rowNum) -> {
+        return new User(rs.getInt("USER_ID"),
+            rs.getString("FIRST_NAME"),
+            rs.getString("LAST_NAME"),
+            rs.getString("EMAIL"),
+            rs.getString("PASSWORD"));
+    });
         
 }
--- src/main/java/com/expensetracker/expensetrackerapi/resources/UserResource.java
+++ src/main/java/com/expensetracker/expensetrackerapi/resources/UserResource.java
@@ -1,7 +1,14 @@
 package com.expensetracker.expensetrackerapi.resources;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import com.expensetracker.expensetrackerapi.domain.User;
+import com.expensetracker.expensetrackerapi.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +18,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserResource
 {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> loginUser(@RequestBody Map<String, Object> userMap)
+    {
+        String email = (String)userMap.get("email");
+        String password = (String)userMap.get("password");
+        User user = userService.validateUser(email, password);
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Login Successful");
+        return new ResponseEntity<>(map, HttpStatus.OK);
+
+    }
+
     @PostMapping("/register")
-    public String registerUser(@RequestBody Map<String, Object> userMap)
+    public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap)
     {
         String firstName = (String) userMap.get("firstname");
         String lastName = (String) userMap.get("lastname");
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
             
-        return firstName + ", " + lastName + ", " + email + ", " + password;
+        User user = userService.registerUser(firstName, lastName, email, password);
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Registered Successfuly");
+
+        // return firstName + ", " + lastName + ", " + email + ", " + password;
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
